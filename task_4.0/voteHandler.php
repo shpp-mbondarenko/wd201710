8818b0ee2780fbc9dcd2cabab7b8a35cbdf811a6
Maxim Bondarenko <maximbndrnk@ukr.net>
2017-11-10 16:59:04 +0200

    write in json file

--- task_4.0/voteHandler.php
+++ task_4.0/voteHandler.php
@@ -1,9 +1,31 @@
 <?php 
-	if (isset($_POST['radiobutton']))
-	{
-		echo '<p>Был выбран независимый переключатель со следующим значением: ';
-		if ($_POST['pikachu']==="pikachu") echo "<b>pikachu</b>";
-		if ($_POST['bulbasaur']==="no") echo "<b>bulbasaur</b>";
-		echo '</p>';
+	$fileName = 'results.json';
+	$arr_data = array(); 
+	try {
+	   //Get form data
+		$pokemon = "";
+	   if (isset($_POST['choosePokemon'])) {
+		$pokemon = $_POST['choosePokemon'];
+	   }
+
+	   //Get data from existing json file
+	   $jsondata = file_get_contents($fileName);
+
+	   // converts json data into array	   
+	   $arr_data = json_decode($jsondata, true);
+
+	   $arr_data[$pokemon] = $arr_data[$pokemon] + 1;
+
+	   //Convert updated array to JSON
+	   $jsondata = json_encode($arr_data, JSON_PRETTY_PRINT);
+	   
+	   //write json data into data.json file
+	   if(file_put_contents($fileName, $jsondata)) {
+	        echo 'Data successfully saved';
+	    } else {
+	        echo "error";
+	    }
+	} catch (Exception $e) {
+	        echo 'Caught exception: ',  $e->getMessage(), "\n";
 	}
 ?>

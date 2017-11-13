1210342ab4684d7bd57edb5373dd9f7eb20e9e9a
Maxim Bondarenko <maximbndrnk@ukr.net>
2017-11-13 17:18:24 +0200

    fix jquery task

--- task_3.0/scripts.js
+++ task_3.0/scripts.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
+  var arr = ['item1', 'item2', 'item3', 'item4', 'item5'];
+  var container = $('#container');
+  createSelect(arr, container);
+
+
   $(".dispNone").hide();
 
   $(".dispNone").hover(
@@ -12,6 +17,30 @@ $(document).ready(function(){
 
 });
 
+//arr - input datf
+//parent in which container to put select
+function createSelect(arr, parent) {
+  //create header of select
+  var customSelectDiv = $('<div class="customSelect"></div>');
+  var cSHeaderDiv = $('<div class="CSHeader"></div>');
+  cSHeaderDiv.append('<img id="headImg" src="img/pokeball.png">');
+  cSHeaderDiv.append('<p class="chosenOption" onclick="showOptions()">Choose item...</p>');
+  cSHeaderDiv.append('<p class="clearSelection" onclick="clearSelection()">&#215;</p>');
+  cSHeaderDiv.append('<p onclick="showOptions()">&#9660;</p>');
+  customSelectDiv.append(cSHeaderDiv);
+  //add elements to custom select
+  var optionsDiv = $('<div class="options"></div>');
+  for (var i = 0, len = arr.length; i < len; i++) {
+    var itemDiv = $('<div class="dispNone" onclick="onOptionClick(this)"></div>');
+    itemDiv.append('<img class="greyimg" src="img/pikachu.png">');
+    itemDiv.append('<p onclick="" class="CSItem">' + arr[i] +'</p>');
+    optionsDiv.append(itemDiv);    
+  }  
+  customSelectDiv.append(optionsDiv);
+  parent.append(customSelectDiv);
+  parent.addClass('container');
+}
+
 function showOptions() {
   $(".dispNone").slideToggle(500);
 }
@@ -22,15 +51,17 @@ function outOptions() {
 }
 
 function clearSelection() {
-  $('.chosenOption').text("Choose pokemon...");
+  $('.chosenOption').text("Choose item...");
   $('.clearSelection').css('visibility', 'hidden');
   $('#headImg').attr("src", "img\/pokeball.png");
 }
 
-function onDivClick(element) {
+//selecting option
+function onOptionClick(element) {
   let str = $(element).find(".CSItem").html();
   $('.chosenOption').text(str);
-  $('#headImg').attr("src", "img\/"+str.toLowerCase()+".png");
+  // $('#headImg').attr("src", "img\/" + str.toLowerCase() + ".png");
+  $('#headImg').attr("src", "img\/pokeball.png");
   $('.clearSelection').css('visibility', 'visible');
   $(".dispNone").slideToggle(500);
 }

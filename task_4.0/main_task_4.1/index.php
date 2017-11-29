52cae2c96d03daeebf1f65c3b26d6c1aa7e76c16
Maxim Bondarenko <maximbndrnk@ukr.net>
2017-11-29 10:22:31 +0200

    php task done

--- task_4.0/main_task_4.1/index.php
+++ task_4.0/main_task_4.1/index.php
@@ -7,21 +7,51 @@
   <link rel="stylesheet" href="styles.css">
   <title>PHP TASK</title>
 </head>
-<body>  
+<body>   
   <div id="topButtons" >
     <a class="button" href='index.php?calculateSum1000=true'><button>Calculate sum -1000 to 1000</button></a>
     <a class="button" href='index.php?calculateSum1000end237=true'><button>Calculate sum -1000 to 1000 end with 2, 3, 7</button></a>
     <a class="button" href='index.php?draw50Stars=true'><button>Draw stars</button></a>
-    <a class="button" href='index.php?drawChessBoard=true'><button>Draw Chess board</button></a>
     <div>
-      <form name="form" action="" method="post">
-        Enter num: <input type="text" name="inputNum" id="inputNum"><br>
-      </form>
-      <a class="button" href='index.php?sumOfNums=true'><button>Calculate sum of nums</button></a>
+      <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+        Enter num of rows: <input type="text" name="row" id="row"><br>    
+        Enter num of columns: <input type="text" name="col" id="col"><br>    
+        <input class="buttonSubmit" type="submit" name="submit" value="Draw chess board">
+      </form>       
+    </div>
+    <div>
+      <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">
+        Enter num: <input type="text" name="inputNum" id="inputNum"><br>    
+        <input class="buttonSubmit" type="submit" name="submit" value="Calculate sum of nums">
+      </form>       
     </div>
     <a class="button" href='index.php?generateArray=true'><button>Generate array</button></a>
   </div>
   <div id="forChessBoard"></div>
+  <?php
+      $str = '';
+      $rows = ''; 
+      $cols = '';
+      if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (isset($_POST['inputNum'])) {
+          $str = testInput($_POST["inputNum"]);
+          sumOfNums($str);
+        }
+        if (isset($_POST['row']) && isset($_POST['col'])) {
+          $rows = testInput($_POST['row']); 
+          $cols = testInput($_POST['col']);
+          drawChessBoard($rows, $cols);
+        }
+      }
+
+      function testInput($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+      }
+    ?> 
+
   <?php
     function calculateSum1000() {
       $sum = 0;
@@ -56,20 +86,40 @@
       }
     }
 
-    function drawChessBoard($rows, $col) {
-      $doc = new DomDocument();
-      $parent = $doc->getElementById('forChessBoard');
-      for ($i = 1; $i <= $rows; $i++) {
-        for ($j = 1; $j <= $col; $j++) {
-          $child = $doc->createElement('div','');
-          $parent->appendChild($child);
+    function drawChessBoard($rows, $cols) {
+      $rows = preg_replace("/[^\d]+/", "", $rows);
+      $cols = preg_replace("/[^\d]+/", "", $cols);
+      $boolVar = true;
+      if ($rows>100 || $cols>100) {
+        return;
+      }
+      for ($i = 0; $i < $rows; $i++) {
+      echo '<div style="display:flex;flex-direction:row;">';
+      if ($cols % 2 == 0) {
+        $boolVar = !$boolVar;
+      }
+      for ($j = 0; $j < $cols; $j++) {
+        if ($boolVar) {
+          echo '<div class="chessB"></div>';
+          $boolVar = !$boolVar;
+        } else {
+          echo '<div class="chessW"></div>';
+          $boolVar = !$boolVar;
         }
-        echo $doc->saveXML();
+      }
+      echo '</div>';
       }
     }
+
     function sumOfNums($str) {
-      echo 'sumOfNums';
+      $sum = 0;
+      $str = preg_replace("/[^\d]+/","",$str);
+      for ($i=0; $i < strlen($str); $i++) { 
+       $sum += $str[$i];
+      }
+      echo 'Sum of nums is - ' . $sum;
     }
+
     function generateArray() {
       $arr = [100]; 
       $res = []; 
@@ -95,15 +145,7 @@
     if (isset($_GET['draw50Stars'])) {
       draw50Stars();
     }
-    if (isset($_GET['drawChessBoard'])) {
-      $rows = $_GET['rows']; 
-      $col = $_GET['col'];
-      drawChessBoard($rows,$col);
-    }
-    if (isset($_GET['sumOfNums'])) {
-      echo $str . " - sss";
-      sumOfNums($str);
-    }
+       
     if (isset($_GET['generateArray'])) {
       generateArray();
     }

6863b9bb0c09aa8d52d2085d2cdcae9f950797c9
Maxim Bondarenko <maximbndrnk@gmail.com>
2017-12-04 17:06:24 +0200

    ajax task in progress

---
+++ task_5.0/saveUser.php
@@ -0,0 +1,30 @@
+<?php
+try {
+  $user = $_POST["user"];
+  $pswd = $_POST["pswd"];
+  $fileName = 'json/users.json';
+  $arr_data = array();
+
+  //Get data from existing json file
+  $jsondata = file_get_contents($fileName);
+  // echo "jsondata - " . $jsondata . '<br>';
+  // converts json data into array
+  $arr_data = json_decode($jsondata, true);
+  //put new data
+  $newUser = array();
+  $newUser['name'] = $user;
+  $newUser['password'] = $pswd;
+  $str = count($arr_data)+'';
+  $arr_data[$str] = $newUser;
+  //Convert updated array to JSON
+  $jsondata = json_encode($arr_data, JSON_PRETTY_PRINT);
+  //write json data into data.json file
+  if(file_put_contents($fileName, $jsondata)) {
+  } else {
+    echo "error. JSON not writed!";
+  }
+} catch (Exception $e) {
+  echo 'Caught exception: ',  $e->getMessage(), "\n";
+}
+
+?>
--- task_5.0/scripts/scripts.js
+++ task_5.0/scripts/scripts.js
@@ -1,8 +1,48 @@
-$('#loginButton').click(function() {
-  console.log("in func!");
-  $.getJSON("json/users.json", function(obj) {
-    $.each(obj, function(key,val) {
-      $('#hint').append("<p>"+ val.name +"</p>");
-    });
-  });
+$(document).ready(function(){
+  console.log("hi!");
+  // $('#loginButton').click(login());
 });
+
+function login() {
+  let user = $('#inputName').val();
+  let pswd = $('#inputPassword').val();
+  var isFindUser = false;
+  console.log("in func! user - " + user+" pswd - "+pswd);
+  $.getJSON("json/users.json", function(obj) {
+    for (let i = 0, len = obj.length; i < len; i++) {
+      console.log("i= "+ i+ "obj="+obj[i].name);
+      if (user === obj[i].name && pswd === obj[i].password) {
+          $('#hint').append('u are login!');
+          isFindUser = true;
+          console.log("isFindUser in "+ isFindUser);
+          break;
+        }
+    }
+    console.log("isFindUser out "+ isFindUser);
+    if (!isFindUser) {
+      //add new user
+      saveUser(user, pswd);
+    }
+  });
+  // let obj = $.getJSON("json/users.json");
+  // console.log("object = "+obj);
+  // console.log("object = "+obj.length);
+}
+
+function saveUser(user, pswd) {
+  var dataString =
+    'user='+ user +
+    '&pswd='+ pswd;
+
+    // AJAX Code To Submit Form.
+  $.ajax({
+      type: "POST",
+      url: "saveUser.php",
+      data: dataString,
+      cache: false,
+      success: function(result){
+        $('#hint').append('<p>user added!'+ result+'</p>');
+        setTimeout("$('#hint').text()",1000);
+      }
+  });
+}
